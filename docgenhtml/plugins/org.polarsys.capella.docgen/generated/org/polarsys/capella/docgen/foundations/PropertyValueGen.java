4f1490f32696a68805d19bc9b9cc2671d8905238
Aurelien Pinsonneau <aurelien.pinsonneau@areva.com>
2018-01-03 15:12:34 +0100

    [1884] Addition of PVG in html exports
    Bug: 1884
    Change-Id: I87a58f420474057d563a1eec4a6735438c80276c
    Signed-off-by: Aurelien Pinsonneau <aurelien.pinsonneau@areva.com>

--- docgenhtml/plugins/org.polarsys.capella.docgen/generated/org/polarsys/capella/docgen/foundations/PropertyValueGen.java
+++ docgenhtml/plugins/org.polarsys.capella.docgen/generated/org/polarsys/capella/docgen/foundations/PropertyValueGen.java
@@ -1,4 +1,4 @@
-//Generated with EGF 1.4.0.v20160519-0641
+//Generated with EGF 1.5.0.v20170111-0853
 package org.polarsys.capella.docgen.foundations;
 
 import org.eclipse.egf.common.helper.*;
@@ -9,12 +9,10 @@ import org.eclipse.egf.pattern.execution.*;
 import org.eclipse.egf.pattern.query.*;
 import org.eclipse.emf.common.util.*;
 import org.polarsys.capella.core.data.capellacore.AbstractPropertyValue;
-import org.polarsys.capella.core.data.capellacore.BooleanPropertyValue;
-import org.polarsys.capella.core.data.capellacore.EnumerationPropertyValue;
-import org.polarsys.capella.core.data.capellacore.FloatPropertyValue;
-import org.polarsys.capella.core.data.capellacore.IntegerPropertyValue;
-import org.polarsys.capella.core.data.capellacore.StringPropertyValue;
+import org.polarsys.capella.core.data.capellacore.PropertyValueGroup;
+import org.polarsys.capella.docgen.util.CapellaServices;
 import org.polarsys.capella.docgen.util.StringUtil;
+import org.polarsys.capella.docgen.util.pattern.helper.PropertyValueHelper;
 
 public class PropertyValueGen {
 	protected static String nl;
@@ -27,26 +25,22 @@ public class PropertyValueGen {
 	}
 
 	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-	protected final String TEXT_1 = "";
-	protected final String TEXT_2 = NL;
-	protected final String TEXT_3 = NL + "<h2>Properties Value</h2>" + NL + "<table max-width=screen.width>" + NL
-			+ "   <thead> " + NL + "       <tr>" + NL + "       \t   <th>Relation</th>\t" + NL
-			+ "           <th>Name</th>" + NL + "           <th>Value</th>" + NL + "           <th>Description</th>"
-			+ NL + "       </tr>" + NL + "   </thead>" + NL + "   <tbody>" + NL;
-	protected final String TEXT_4 = NL + "   \t   <tr>" + NL + "           <td><i>";
-	protected final String TEXT_5 = "</i></td>" + NL + "           <td>";
-	protected final String TEXT_6 = "</td>" + NL + "           <td>";
-	protected final String TEXT_7 = "</td>" + NL + "           <td>";
-	protected final String TEXT_8 = "</td>" + NL + "       </tr>" + NL;
-	protected final String TEXT_9 = NL;
-	protected final String TEXT_10 = NL + "   \t   <tr>" + NL + "           <td><i>";
-	protected final String TEXT_11 = "</i></td>" + NL + "           <td>";
-	protected final String TEXT_12 = "</td>" + NL + "           <td>";
-	protected final String TEXT_13 = "</td>" + NL + "           <td>";
-	protected final String TEXT_14 = "</td>" + NL + "       </tr>";
-	protected final String TEXT_15 = NL + "   </tbody>" + NL + "</table>";
-	protected final String TEXT_16 = NL;
-	protected final String TEXT_17 = NL;
+	protected final String TEXT_1 = "\t<h2>Property Values</h2>" + NL + "\t<table max-width=screen.width>" + NL
+			+ "\t   <thead> " + NL + "\t       <tr>" + NL + "\t       \t   <th>Relation</th>\t" + NL
+			+ "\t           <th>Name</th>" + NL + "\t           <th>Value</th>" + NL
+			+ "\t           <th>Description</th>" + NL + "\t       </tr>" + NL + "\t   </thead>" + NL + "\t   <tbody>"
+			+ NL + "\t";
+	protected final String TEXT_2 = NL + "\t";
+	protected final String TEXT_3 = NL + "\t</tbody>" + NL + "\t</table>" + NL + "\t";
+	protected final String TEXT_4 = NL + "\t<h2>Property Value Groups</h2>" + NL + "\t<table max-width=screen.width>"
+			+ NL + "\t   <thead> " + NL + "\t       <tr>" + NL + "\t       \t   <th>Relation</th>\t" + NL
+			+ "\t           <th>Name</th>" + NL + "\t           <th>Value</th>" + NL
+			+ "\t           <th>Description</th>" + NL + "\t       </tr>" + NL + "\t   </thead>" + NL + "\t   <tbody>"
+			+ NL + "\t";
+	protected final String TEXT_5 = NL + "\t";
+	protected final String TEXT_6 = NL + "\t</tbody>" + NL + "\t</table>" + NL + "\t";
+	protected final String TEXT_7 = NL;
+	protected final String TEXT_8 = NL;
 
 	public PropertyValueGen() {
 		//Here is the constructor
@@ -92,8 +86,8 @@ public class PropertyValueGen {
 			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
 		}
 
-		stringBuffer.append(TEXT_16);
-		stringBuffer.append(TEXT_17);
+		stringBuffer.append(TEXT_7);
+		stringBuffer.append(TEXT_8);
 		return stringBuffer.toString();
 	}
 
@@ -142,92 +136,39 @@ public class PropertyValueGen {
 
 	protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
 
-		EList<AbstractPropertyValue> applayed_list = element.getAppliedPropertyValues();
-		stringBuffer.append(TEXT_1);
+		// We get the list of applied and owned PV
+		EList<AbstractPropertyValue> applied_list = element.getAppliedPropertyValues();
 		EList<AbstractPropertyValue> owned_list = element.getOwnedPropertyValues();
-		stringBuffer.append(TEXT_2);
-		if ((applayed_list != null && applayed_list.size() > 0) || (owned_list != null && owned_list.size() > 0)) {
+
+		if ((applied_list != null && applied_list.size() > 0) || (owned_list != null && owned_list.size() > 0)) {
+
+			stringBuffer.append(TEXT_1);
+
+			String table = PropertyValueHelper.getPVTable(element, 0, projectName, outputFolder);
+
+			stringBuffer.append(TEXT_2);
+			stringBuffer.append(table);
 			stringBuffer.append(TEXT_3);
-			// Handle Owned PropertyValues if not Handled yet 
-			for (AbstractPropertyValue propertyValue : owned_list) {
-				// Variable initialization
-				String relation = "Contained";
-				String name = propertyValue.getName();
-				String value = "";
-				String description = propertyValue.getDescription();
-				description = StringUtil.transformAREFString(propertyValue, description, projectName, outputFolder);
 
-				if (propertyValue instanceof EnumerationPropertyValue) {
-					if (((EnumerationPropertyValue) propertyValue).getValue() != null)
-						value = ((EnumerationPropertyValue) propertyValue).getValue().getName();
-				}
-
-				if (propertyValue instanceof BooleanPropertyValue)
-					value = (((BooleanPropertyValue) propertyValue).isValue() ? "True" : "False");
-
-				if (propertyValue instanceof StringPropertyValue)
-					value = ((StringPropertyValue) propertyValue).getValue();
-
-				if (propertyValue instanceof IntegerPropertyValue)
-					value = Integer.toString(((IntegerPropertyValue) propertyValue).getValue());
-
-				if (propertyValue instanceof FloatPropertyValue)
-					value = Float.toString(((FloatPropertyValue) propertyValue).getValue());
-
-				if (applayed_list.contains(propertyValue))
-					relation += " &amp; Applied";
-
-				stringBuffer.append(TEXT_4);
-				stringBuffer.append(relation);
-				stringBuffer.append(TEXT_5);
-				stringBuffer.append(name);
-				stringBuffer.append(TEXT_6);
-				stringBuffer.append(value);
-				stringBuffer.append(TEXT_7);
-				stringBuffer.append(description);
-				stringBuffer.append(TEXT_8);
-			}
-			stringBuffer.append(TEXT_9);
-			// Handle Applied PropertyValues 
-			for (AbstractPropertyValue propertyValue : applayed_list) {
-				if (!owned_list.contains(propertyValue)) {
-					// Variable initialization
-					String relation = "Applied";
-					String name = propertyValue.getName();
-					String value = "";
-					String description = propertyValue.getDescription();
-					description = StringUtil.transformAREFString(propertyValue, description, projectName, outputFolder);
-
-					if (propertyValue instanceof EnumerationPropertyValue) {
-						if (((EnumerationPropertyValue) propertyValue).getValue() != null)
-							value = ((EnumerationPropertyValue) propertyValue).getValue().getName();
-					}
-
-					if (propertyValue instanceof BooleanPropertyValue)
-						value = (((BooleanPropertyValue) propertyValue).isValue() ? "True" : "False");
-
-					if (propertyValue instanceof StringPropertyValue)
-						value = ((StringPropertyValue) propertyValue).getValue();
-
-					if (propertyValue instanceof IntegerPropertyValue)
-						value = Integer.toString(((IntegerPropertyValue) propertyValue).getValue());
-
-					if (propertyValue instanceof FloatPropertyValue)
-						value = Float.toString(((FloatPropertyValue) propertyValue).getValue());
-
-					stringBuffer.append(TEXT_10);
-					stringBuffer.append(relation);
-					stringBuffer.append(TEXT_11);
-					stringBuffer.append(name);
-					stringBuffer.append(TEXT_12);
-					stringBuffer.append(value);
-					stringBuffer.append(TEXT_13);
-					stringBuffer.append(description);
-					stringBuffer.append(TEXT_14);
-				}
-			}
-			stringBuffer.append(TEXT_15);
 		}
+
+		// We get the list of applied and owned PVG
+		EList<PropertyValueGroup> appliedgroup_list = element.getAppliedPropertyValueGroups();
+		EList<PropertyValueGroup> ownedgroup_list = element.getOwnedPropertyValueGroups();
+
+		if ((appliedgroup_list != null && appliedgroup_list.size() > 0)
+				|| (ownedgroup_list != null && ownedgroup_list.size() > 0)) {
+
+			stringBuffer.append(TEXT_4);
+
+			String table = PropertyValueHelper.getPVGTable(element, 0, projectName, outputFolder);
+
+			stringBuffer.append(TEXT_5);
+			stringBuffer.append(table);
+			stringBuffer.append(TEXT_6);
+
+		}
+
 		InternalPatternContext ictx = (InternalPatternContext) ctx;
 		new Node.DataLeaf(ictx.getNode(), getClass(), "body", stringBuffer.toString());
 	}
