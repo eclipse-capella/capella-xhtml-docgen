e4d66653ea4307f33dc3992fb3d6917f5555932a
Boubekeur Zendagui <boubekeur.zendagui@thalesgroup.com>
2019-07-05 15:46:32 +0200

    [2539] Contributing with new HTML pages for model elements
    - Integration in Capella xHTML documentation generation
    - Add Open point to contribute to existing pages with requirements
    
    Bug: 2539
    Change-Id: I0024fa7e011def80cffffd721c1fac8053c94b43
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@thalesgroup.com>

--- docgenhtml/plugins/org.polarsys.capella.docgen/generated/org/polarsys/capella/docgen/foundations/CapellaElementContentDocGen.java
+++ docgenhtml/plugins/org.polarsys.capella.docgen/generated/org/polarsys/capella/docgen/foundations/CapellaElementContentDocGen.java
@@ -33,15 +33,16 @@ public class CapellaElementContentDocGen {
 	protected final String TEXT_7 = NL;
 	protected final String TEXT_8 = NL;
 	protected final String TEXT_9 = NL;
-	protected final String TEXT_10 = NL;
-	protected final String TEXT_11 = NL + "<h2>Requirements</h2>" + NL + "" + NL + "<table>" + NL + "\t<tr>" + NL
+	protected final String TEXT_10 = NL + NL + NL + NL;
+	protected final String TEXT_11 = NL + NL + NL + NL;
+	protected final String TEXT_12 = NL + "<h2>Requirements</h2>" + NL + "" + NL + "<table>" + NL + "\t<tr>" + NL
 			+ "\t\t<th>ID</th>" + NL + "\t\t<th>Name</th>" + NL + "\t\t<th>Description</th>" + NL + "\t</tr>" + NL
 			+ "\t";
-	protected final String TEXT_12 = NL + "\t";
 	protected final String TEXT_13 = NL + "\t";
-	protected final String TEXT_14 = NL + "</table>";
-	protected final String TEXT_15 = NL;
+	protected final String TEXT_14 = NL + "\t";
+	protected final String TEXT_15 = NL + "</table>";
 	protected final String TEXT_16 = NL;
+	protected final String TEXT_17 = NL;
 
 	public CapellaElementContentDocGen() {
 		//Here is the constructor
@@ -82,8 +83,8 @@ public class CapellaElementContentDocGen {
 			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
 		}
 
-		stringBuffer.append(TEXT_15);
 		stringBuffer.append(TEXT_16);
+		stringBuffer.append(TEXT_17);
 		return stringBuffer.toString();
 	}
 
@@ -180,19 +181,24 @@ public class CapellaElementContentDocGen {
 		}
 
 		stringBuffer.append(TEXT_10);
+
+		org.polarsys.kitalpha.doc.gen.business.core.extension.intf.DocGenExtensionFactory.newDocGenExtensionEngine()
+				.apply("CAPELLA_REQUIREMENTS_EXTENSION", ctx, getParameters(), stringBuffer);
+
+		stringBuffer.append(TEXT_11);
 		// requirements
 
 		Collection<String> requirementsTableLines = CapellaElementService.getRequirementsToLine(element, projectName,
 				outputFolder);
 		if (requirementsTableLines.size() > 0) {
 
-			stringBuffer.append(TEXT_11);
+			stringBuffer.append(TEXT_12);
 			for (String line : requirementsTableLines) {
-				stringBuffer.append(TEXT_12);
-				stringBuffer.append(line);
 				stringBuffer.append(TEXT_13);
+				stringBuffer.append(line);
+				stringBuffer.append(TEXT_14);
 			}
-			stringBuffer.append(TEXT_14);
+			stringBuffer.append(TEXT_15);
 		}
 		InternalPatternContext ictx = (InternalPatternContext) ctx;
 		new Node.DataLeaf(ictx.getNode(), getClass(), "body", stringBuffer.toString());
