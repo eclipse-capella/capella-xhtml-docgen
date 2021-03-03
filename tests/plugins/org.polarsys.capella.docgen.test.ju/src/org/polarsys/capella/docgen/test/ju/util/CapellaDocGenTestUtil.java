25e543804c096f8f1c10df3de905dd91ee054981
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-03-03 17:52:47 +0100

    #211 Remove non relevant content from test result html content
    - Remove coord attribute in area tag
    The content of the coord attribute generated for cliquable areas of
    diagrams is sensitive depending on the docgen execution environment.
    This attribute content is to be tested in a dedicated api test.
    Update expected test results accordingly.
    
    - Remove all attributes content in area tag
    The content of the map tag generated for clickable areas of
    diagrams is sensitive depending on the docgen execution environment.
    Size of elements variations depending on execution environment may lead
    to different order of the area elements.
    Update expected test results accordingly.
    
    Change-Id: I28128e7dc8e67b5b4ef2ff19d3675ccc9136b6cc
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ tests/plugins/org.polarsys.capella.docgen.test.ju/src/org/polarsys/capella/docgen/test/ju/util/CapellaDocGenTestUtil.java
@@ -0,0 +1,26 @@
+package org.polarsys.capella.docgen.test.ju.util;
+
+public class CapellaDocGenTestUtil {
+	
+	private static final String IMG_SRC_ICON_PNG_REPLACEMENT = "img src=\"$1../icon/$2.png\"";
+	private static final String IMG_SRC_ICON_GIF_PATTERN = "img src=\"(../)?../icon/(.*?).gif\"";
+	private static final String R_N_REPLACEMENT = "\n";
+	private static final String R_N = "\\r\\n";
+
+	public static String formatDocGenOutput(String toBeFormattedGenerationContent) {
+		String result = toBeFormattedGenerationContent;
+		
+		// Replace line endings
+		result = result.replaceAll(R_N, R_N_REPLACEMENT);
+		
+		// Set all icon files extensions to png
+		result = result.replaceAll(IMG_SRC_ICON_GIF_PATTERN, IMG_SRC_ICON_PNG_REPLACEMENT);
+		
+		// Remove area coords content (may differ a bit depending on the diagram rendering, the OS, the font availability...) 
+		//<area shape="rect" group="bbee94fd1bf74709bc8e3de6dfe74908" coords="226,157,242,173" href="../IFE/bbee94fd1bf74709bc8e3de6dfe74908.html#idbbee94fd-1bf7-4709-bc8e-3de6dfe74908" alt="bbee94fd1bf74709bc8e3de6dfe74908" />
+		result = result.replaceAll("area shape=\"(.*?)\" group=\"(.*?)\" coords=\"(.*?)\" href=\"(.*?)\" alt=\"(.*?)\"", "area shape=\"\" group=\"\" coords=\"\" href=\"\" alt\"\"");
+		
+		return result;
+	}
+
+}
