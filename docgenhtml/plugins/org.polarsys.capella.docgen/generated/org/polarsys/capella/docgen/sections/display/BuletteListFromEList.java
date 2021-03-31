91f3d77acd97d87dfe087a23f903d66bfe72e4ee
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-03-31 09:47:04 +0200

    Pattern section refactoring (AbstractFunction)
    - AbstractionFunctionDocGen refactoring. Split template content into
    section-specific templates.
    - Add BuletteList and Table generic templates.
    - Add AbstractFunction focused test model.
    
    - Add .keep file to keep src folder
    - Add .gitignore rule for keep files
    
    Change-Id: Ia49fcf6835dccbc049b478a56cbd10f054f3985d

---
+++ docgenhtml/plugins/org.polarsys.capella.docgen/generated/org/polarsys/capella/docgen/sections/display/BuletteListFromEList.java
@@ -0,0 +1,140 @@
+//Generated with EGF 1.6.2.202001031546
+package org.polarsys.capella.docgen.sections.display;
+
+import java.util.*;
+import org.eclipse.egf.model.pattern.*;
+import org.eclipse.egf.pattern.execution.*;
+import org.eclipse.egf.pattern.query.*;
+import org.polarsys.capella.docgen.util.StringUtil;
+
+public class BuletteListFromEList extends org.polarsys.capella.docgen.sections.display.TitleSection {
+	protected static String nl;
+
+	public static synchronized BuletteListFromEList create(String lineSeparator) {
+		nl = lineSeparator;
+		BuletteListFromEList result = new BuletteListFromEList();
+		nl = null;
+		return result;
+	}
+
+	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+	protected final String TEXT_1 = "";
+	protected final String TEXT_2 = NL;
+
+	public BuletteListFromEList() {
+		//Here is the constructor
+		StringBuffer stringBuffer = new StringBuffer();
+
+		// add initialisation of the pattern variables (declaration has been already done).
+
+	}
+
+	public String generate(Object argument) throws Exception {
+		final StringBuffer stringBuffer = new StringBuffer();
+
+		InternalPatternContext ctx = (InternalPatternContext) argument;
+		Map<String, String> queryCtx = null;
+		IQuery.ParameterDescription paramDesc = null;
+		Node.Container currentNode = ctx.getNode();
+
+		List<Object> projectNameParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> outputFolderParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> parameterListParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> sectionTitleParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> sectionLevelParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+
+		for (Object projectNameParameterParameter : projectNameParameterList) {
+			for (Object outputFolderParameterParameter : outputFolderParameterList) {
+				for (Object parameterListParameterParameter : parameterListParameterList) {
+					for (Object sectionTitleParameterParameter : sectionTitleParameterList) {
+						for (Object sectionLevelParameterParameter : sectionLevelParameterList) {
+
+							this.projectNameParameter = (java.lang.String) projectNameParameterParameter;
+							this.outputFolderParameter = (java.lang.String) outputFolderParameterParameter;
+							this.parameterListParameter = (org.eclipse.emf.common.util.EList) parameterListParameterParameter;
+							this.sectionTitleParameter = (java.lang.String) sectionTitleParameterParameter;
+							this.sectionLevelParameter = (java.lang.Integer) sectionLevelParameterParameter;
+
+							if (preCondition(ctx)) {
+								ctx.setNode(new Node.Container(currentNode, getClass()));
+								orchestration(ctx);
+							}
+
+						}
+					}
+				}
+			}
+		}
+		ctx.setNode(currentNode);
+		if (ctx.useReporter()) {
+			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
+		}
+
+		stringBuffer.append(TEXT_2);
+		stringBuffer.append(TEXT_2);
+		return stringBuffer.toString();
+	}
+
+	public String orchestration(PatternContext ctx) throws Exception {
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+
+		super.orchestration(new SuperOrchestrationContext(ictx));
+
+		method_body(new StringBuffer(), ictx);
+
+		if (ictx.useReporter()) {
+			Map<String, Object> parameterValues = new HashMap<String, Object>();
+			parameterValues.put("projectNameParameter", this.projectNameParameter);
+			parameterValues.put("outputFolderParameter", this.outputFolderParameter);
+			parameterValues.put("parameterListParameter", this.parameterListParameter);
+			parameterValues.put("sectionTitleParameter", this.sectionTitleParameter);
+			parameterValues.put("sectionLevelParameter", this.sectionLevelParameter);
+			String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
+			String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
+			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+		}
+		return null;
+	}
+
+	protected java.lang.String projectNameParameter = null;
+
+	public void set_projectNameParameter(java.lang.String object) {
+		this.projectNameParameter = object;
+	}
+
+	protected java.lang.String outputFolderParameter = null;
+
+	public void set_outputFolderParameter(java.lang.String object) {
+		this.outputFolderParameter = object;
+	}
+
+	protected org.eclipse.emf.common.util.EList parameterListParameter = null;
+
+	public void set_parameterListParameter(org.eclipse.emf.common.util.EList object) {
+		this.parameterListParameter = object;
+	}
+
+	public Map<String, Object> getParameters() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("projectNameParameter", this.projectNameParameter);
+		parameters.put("outputFolderParameter", this.outputFolderParameter);
+		parameters.put("parameterListParameter", this.parameterListParameter);
+		parameters.put("sectionTitleParameter", this.sectionTitleParameter);
+		parameters.put("sectionLevelParameter", this.sectionLevelParameter);
+		return parameters;
+	}
+
+	protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
+
+		stringBuffer.append(
+				StringUtil.stringListToBulette(parameterListParameter, projectNameParameter, outputFolderParameter));
+		stringBuffer.append(TEXT_1);
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+		new Node.DataLeaf(ictx.getNode(), getClass(), "body", stringBuffer.toString());
+	}
+}
---
+++ docgenhtml/plugins/org.polarsys.capella.docgen/generated/org/polarsys/capella/docgen/sections/template/templatePattern.java
@@ -0,0 +1,154 @@
+//Generated with EGF 1.6.2.202001031546
+package org.polarsys.capella.docgen.sections.template;
+
+import java.util.*;
+import org.eclipse.egf.model.pattern.*;
+import org.eclipse.egf.pattern.execution.*;
+import org.eclipse.egf.pattern.query.*;
+
+public class templatePattern {
+	protected static String nl;
+
+	public static synchronized templatePattern create(String lineSeparator) {
+		nl = lineSeparator;
+		templatePattern result = new templatePattern();
+		nl = null;
+		return result;
+	}
+
+	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+	protected final String TEXT_1 = "%>";
+	protected final String TEXT_2 = NL;
+
+	public templatePattern() {
+		//Here is the constructor
+		StringBuffer stringBuffer = new StringBuffer();
+
+		// add initialisation of the pattern variables (declaration has been already done).
+
+	}
+
+	public String generate(Object argument) throws Exception {
+		final StringBuffer stringBuffer = new StringBuffer();
+
+		InternalPatternContext ctx = (InternalPatternContext) argument;
+		Map<String, String> queryCtx = null;
+		IQuery.ParameterDescription paramDesc = null;
+		Node.Container currentNode = ctx.getNode();
+
+		List<Object> parameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> projectNameParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> outputFolderParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> elementNameParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> elementTypeParameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+
+		for (Object parameterParameter : parameterList) {
+			for (Object projectNameParameterParameter : projectNameParameterList) {
+				for (Object outputFolderParameterParameter : outputFolderParameterList) {
+					for (Object elementNameParameterParameter : elementNameParameterList) {
+						for (Object elementTypeParameterParameter : elementTypeParameterList) {
+
+							this.parameter = (org.polarsys.capella.core.data.fa.AbstractFunction) parameterParameter;
+							this.projectNameParameter = (java.lang.String) projectNameParameterParameter;
+							this.outputFolderParameter = (java.lang.String) outputFolderParameterParameter;
+							this.elementNameParameter = (java.lang.String) elementNameParameterParameter;
+							this.elementTypeParameter = (java.lang.String) elementTypeParameterParameter;
+
+							if (preCondition(ctx)) {
+								ctx.setNode(new Node.Container(currentNode, getClass()));
+								orchestration(ctx);
+							}
+
+						}
+					}
+				}
+			}
+		}
+		ctx.setNode(currentNode);
+		if (ctx.useReporter()) {
+			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
+		}
+
+		stringBuffer.append(TEXT_2);
+		stringBuffer.append(TEXT_2);
+		return stringBuffer.toString();
+	}
+
+	public String orchestration(PatternContext ctx) throws Exception {
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+
+		method_body(new StringBuffer(), ictx);
+
+		if (ictx.useReporter()) {
+			Map<String, Object> parameterValues = new HashMap<String, Object>();
+			parameterValues.put("parameter", this.parameter);
+			parameterValues.put("projectNameParameter", this.projectNameParameter);
+			parameterValues.put("outputFolderParameter", this.outputFolderParameter);
+			parameterValues.put("elementNameParameter", this.elementNameParameter);
+			parameterValues.put("elementTypeParameter", this.elementTypeParameter);
+			String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
+			String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
+			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+		}
+		return null;
+	}
+
+	protected org.polarsys.capella.core.data.fa.AbstractFunction parameter = null;
+
+	public void set_parameter(org.polarsys.capella.core.data.fa.AbstractFunction object) {
+		this.parameter = object;
+	}
+
+	protected java.lang.String projectNameParameter = null;
+
+	public void set_projectNameParameter(java.lang.String object) {
+		this.projectNameParameter = object;
+	}
+
+	protected java.lang.String outputFolderParameter = null;
+
+	public void set_outputFolderParameter(java.lang.String object) {
+		this.outputFolderParameter = object;
+	}
+
+	protected java.lang.String elementNameParameter = null;
+
+	public void set_elementNameParameter(java.lang.String object) {
+		this.elementNameParameter = object;
+	}
+
+	protected java.lang.String elementTypeParameter = null;
+
+	public void set_elementTypeParameter(java.lang.String object) {
+		this.elementTypeParameter = object;
+	}
+
+	public Map<String, Object> getParameters() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("parameter", this.parameter);
+		parameters.put("projectNameParameter", this.projectNameParameter);
+		parameters.put("outputFolderParameter", this.outputFolderParameter);
+		parameters.put("elementNameParameter", this.elementNameParameter);
+		parameters.put("elementTypeParameter", this.elementTypeParameter);
+		return parameters;
+	}
+
+	protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
+
+		//	String title = "Allocating Component";
+		//	Integer level = 2;
+		// <%@ egf:patternCall patternId="platform:/plugin/org.polarsys.capella.docgen/egf/HTMLDocGenCapella.fcore#LogicalName=org.polarsys.capella.docgen.sections.display.BuletteList" args="allocatingComponents:parameterListParameter, projectNameParameter:projectNameParameter, outputFolderParameter:outputFolderParameter, title:sectionTitleParameter, level:sectionLevelParameter"
+		stringBuffer.append(TEXT_1);
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+		new Node.DataLeaf(ictx.getNode(), getClass(), "body", stringBuffer.toString());
+	}
+
+	public boolean preCondition(PatternContext ctx) throws Exception {
+		return true;
+	}
+}
