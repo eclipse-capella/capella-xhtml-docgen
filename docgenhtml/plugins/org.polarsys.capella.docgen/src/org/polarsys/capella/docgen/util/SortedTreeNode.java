092be3069344f021dbebdf40db782d2bc09a48b6
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-04-01 15:42:30 +0200

    #155 Fix sonar issues
    Change-Id: I0000000000000000000000000000000000000000

--- docgenhtml/plugins/org.polarsys.capella.docgen/src/org/polarsys/capella/docgen/util/SortedTreeNode.java
+++ docgenhtml/plugins/org.polarsys.capella.docgen/src/org/polarsys/capella/docgen/util/SortedTreeNode.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 THALES GLOBAL SERVICES.
+ * Copyright (c) 2019, 2021 THALES GLOBAL SERVICES.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
@@ -13,7 +13,6 @@
 package org.polarsys.capella.docgen.util;
 
 import java.util.Comparator;
-import java.util.Vector;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
@@ -24,33 +23,79 @@ import javax.swing.tree.MutableTreeNode;
  */
 @SuppressWarnings("serial")
 public class SortedTreeNode extends DefaultMutableTreeNode {
-	
-    /** Force type of children */
-    protected Vector<DefaultMutableTreeNode> children;
-	
+
 	private final Comparator<DefaultMutableTreeNode> comparator;
-	
+
 	public SortedTreeNode(Comparator<DefaultMutableTreeNode> comparator) {
 		super();
 		this.comparator = comparator;
 	}
-	
+
 	public SortedTreeNode(Object userObject, Comparator<DefaultMutableTreeNode> comparator) {
 		super(userObject);
 		this.comparator = comparator;
 	}
-	
+
 	public SortedTreeNode(Object userObject, boolean allowsChildren, Comparator<DefaultMutableTreeNode> comparator) {
 		super(userObject, allowsChildren);
 		this.comparator = comparator;
 	}
-	
+
 	@Override
-	public void add(MutableTreeNode newChild) {
-		super.add(newChild);
-		if (this.comparator != null && this.children != null) {
-			this.children.sort(this.comparator);
+	/**
+	 * Override insert method to ensure ordering of elements. Actual insert is done
+	 * by the parent method but {@code childIndex} is replaced by a computed value.
+	 */
+	public void insert(MutableTreeNode newChild, int childIndex) {
+		int insertIndex = 0;
+		// Check limit cases
+		if (this.children == null || this.children.size() == 0) {
+			super.insert(newChild, insertIndex);
+			return;
 		}
+
+		// Compute the {@code insertIndex} value by dichotomy.
+		// Keeping track of a min and max index between which {@code newChild} is to be inserted.
+		int minIndex = 0;
+		int maxIndex = this.children.size();
+		boolean found = false;
+		while (!found) {
+			// Compute insertIndex (here it is equal to the middle of the remaining elements).
+			insertIndex = minIndex + ((maxIndex - minIndex) / 2);
+			// Compare with element located at insertIndex
+			int compareResult = this.comparator.compare((DefaultMutableTreeNode) newChild,
+					(DefaultMutableTreeNode) this.children.get(insertIndex));
+			if (compareResult == 0) {
+				// If equal then this is good
+				found = true;
+			} else if (compareResult < 0) {
+				// If smaller
+				if (insertIndex == maxIndex) {
+					// min and max are so close that rounding the index value gives the same value
+					// So we have found but newChild should be inserted before
+					found = true;
+					insertIndex = minIndex;
+				} else {
+					// Narrow the max index
+					maxIndex = insertIndex;
+				}
+			} else {
+				if (insertIndex == minIndex) {
+					// min and max are so close that rounding the index value gives the same value
+					// So we have found but newChild should be inserted after
+					found = true;
+					insertIndex = maxIndex;
+				}
+				// Narrow the min index
+				minIndex = insertIndex;
+			}
+			if (maxIndex == minIndex) {
+				insertIndex = minIndex;
+				found = true;
+			}
+		}
+		// insert using standard code
+		super.insert(newChild, insertIndex);
 	}
 
 }
