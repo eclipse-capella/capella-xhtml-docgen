414a441c2ecd6199350cbd5d8bd4e8b9ebbf88e5
Erwann Traisnel <erwann.traisnel@obeo.fr>
2024-11-25 17:36:31 +0000

    #271 Fix "Inheriting from" and "Inherited by" links
    Sections were not generated in doc
    
    Added test case in local IFE
    
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- docgenhtml/plugins/org.polarsys.capella.docgen/src/org/polarsys/capella/docgen/util/GeneralizableElementServices.java
+++ docgenhtml/plugins/org.polarsys.capella.docgen/src/org/polarsys/capella/docgen/util/GeneralizableElementServices.java
@@ -16,60 +16,61 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.polarsys.capella.core.data.capellacore.GeneralizableElement;
-import org.polarsys.capella.core.data.capellacore.impl.GeneralizableElementImpl;
 
 /**
  * 
  */
 public class GeneralizableElementServices {
-	/**
-	 * <b>Get the list of subclasses of a class</b>
-	 * <p>
-	 * Get the list of subclasses of a class
-	 * 
-	 * @param eObject_p
-	 * @return
-	 */
-	public static List<String> getClassInheritedBy(EObject eObject_p, String projectName, String outputFolder) {
-		// Create the list to return
-		List<String> ret = new ArrayList<String>();
-		// If eObject is a Class
-		if (eObject_p instanceof GeneralizableElementImpl) {
-			// For each sub Generalizable Element
-			for (GeneralizableElement elt : ((GeneralizableElementImpl) eObject_p).getSub()) {
-				// If the element is a Class
-				if (elt instanceof GeneralizableElementImpl) {
-					// Add the class to the list to return
-					ret.add(CapellaServices.getImageLinkFromElement(elt, projectName, outputFolder) + " " + CapellaServices.getFullDataPkgHierarchyLink(elt));
-				}
-			}
-		}
-		return ret;
-	}
+  /**
+   * <b>Get the list of subclasses of a class</b>
+   * <p>
+   * Get the list of subclasses of a class
+   * 
+   * @param eObject_p
+   * @return
+   */
+  public static List<String> getClassInheritedBy(EObject eObject_p, String projectName, String outputFolder) {
+    // Create the list to return
+    List<String> ret = new ArrayList<String>();
+    // If eObject is a Class
+    if (eObject_p instanceof GeneralizableElement) {
+      // For each sub Generalizable Element
+      for (GeneralizableElement elt : ((GeneralizableElement) eObject_p).getSub()) {
+        // If the element is a Class
+        if (elt instanceof GeneralizableElement) {
+          // Add the class to the list to return
+          ret.add(CapellaServices.getImageLinkFromElement(elt, projectName, outputFolder) + " "
+              + CapellaServices.getFullDataPkgHierarchyLink(elt));
+        }
+      }
+    }
+    return ret;
+  }
 
-	/**
-	 * <b>Get the list of super classes of a class</b>
-	 * <p>
-	 * Get the list of super classes of a class
-	 * 
-	 * @param eObject_p
-	 * @return
-	 */
-	public static List<String> getClassInheritingFrom(EObject eObject_p, String projectName, String outputFolder) {
-		// Create the list to return
-		List<String> ret = new ArrayList<String>();
+  /**
+   * <b>Get the list of super classes of a class</b>
+   * <p>
+   * Get the list of super classes of a class
+   * 
+   * @param eObject_p
+   * @return
+   */
+  public static List<String> getClassInheritingFrom(EObject eObject_p, String projectName, String outputFolder) {
+    // Create the list to return
+    List<String> ret = new ArrayList<String>();
 
-		if (eObject_p instanceof GeneralizableElementImpl) {
-			// For each super Generalizable Element
-			for (GeneralizableElement elt : ((GeneralizableElementImpl) eObject_p).getSuper()) {
-				// If the element is a Class
-				if (elt instanceof GeneralizableElementImpl) {
-					// Add the class to the list to return
-					ret.add(CapellaServices.getImageLinkFromElement(elt, projectName, outputFolder) + " " + CapellaServices.getFullDataPkgHierarchyLink(elt));
-				}
-			}
-		}
-		return ret;
-	}
+    if (eObject_p instanceof GeneralizableElement) {
+      // For each super Generalizable Element
+      for (GeneralizableElement elt : ((GeneralizableElement) eObject_p).getSuper()) {
+        // If the element is a Class
+        if (elt instanceof GeneralizableElement) {
+          // Add the class to the list to return
+          ret.add(CapellaServices.getImageLinkFromElement(elt, projectName, outputFolder) + " "
+              + CapellaServices.getFullDataPkgHierarchyLink(elt));
+        }
+      }
+    }
+    return ret;
+  }
 
 }
